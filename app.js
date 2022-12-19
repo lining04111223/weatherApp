f5a786cc5ec4ddf1034e33e3cfb4f3792be6cc71
Ning Li <lining04111223@gmail.com>
2022-12-18 16:15:07 -0800

    get request

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+//api key
+const myKey = "23c557af9d805288d5a0b1fceda6f258";
+
 const express = require("express");
 const app = express();
-const ejs = require;
+const ejs = require("ejs");
+const https = require("node:https");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/:city", (req, res) => {
+  let { city } = req.params;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`;
+  console.log(req.params);
+  //get request made by node.js
+  https
+    .get(url, (response) => {
+      console.log("statusCode:", response.statusCode);
+      console.log("headers:", response.headers);
+
+      response.on("data", (d) => {
+        let djs = JSON.parse(d);
+        console.log("djs", djs);
+        res.render("weather.ejs", { djs });
+      });
+    })
+    .on("error", (e) => {
+      console.error(e);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
